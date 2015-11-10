8729205b00321630f3f3436b4a1c92f230c57e83
mbates7 <hotfortaturtle@gmail.com>
2015-11-10 02:23:21 +0000

    ,

---
+++ HW9.1Questions.md
@@ -0,0 +1,16 @@
+
+What did we do well that we should try to do again?
+
+
+
+
+What did we do poorly that we should try to correct?
+
+
+
+
+
+Did we achieve our goals?
+Why or why not?
+
+
---
+++ Homework-9.1.js
@@ -0,0 +1,29 @@
+
+/*console.assert(add(1,1)==2,"add function failed"); 
+console.assert(add(1,1)==1,"add function failed");
+
+function add(x,y){
+    return Number.Nan;
+}*/
+ 
+ 
+ 
+console.assert(sub(3,2)==1,"sub function failed");
+console.assert(sub(3,2)==2,"sub function failed");
+
+function sub(x,y){
+    return Number.Nan;
+}
+
+console.assert(mult(2,4)==8);
+console.assert(mult(2,4)==5);
+console.assert(mult(9,0)==0);
+
+
+
+console.assert(div(4,2)==2);
+console.assert(div(4,2)==1);
+ 
+
+/* Michael, John, Giang
+
