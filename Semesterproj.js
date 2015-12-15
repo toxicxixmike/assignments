8574c2b019a87e220b0c707f0a3321dee0756cac
mbates7 <hotfortaturtle@gmail.com>
2015-12-15 00:22:36 +0000

    Project

--- Semesterproj.js
+++ Semesterproj.js
@@ -3,12 +3,33 @@ var Person = function (fName) {
 };    
 
 Person.prototype.sayHello = function() {
-  console.log("Hello, I am " + this.firstName);
+      console.log(this.firstname + "This is my post");
 }
-var person1 = new Person('Michael') ;
-var helloFunction = person1.sayThisisapost
+  var person1 = new Person('Michael') ;
+  var person2 = new Person('Joe') ;
+  var person3 = new Person('Natalia');
+  var person4 = new Person('Spoof');
+  var person5 = new Person('Dustin');
+  var person6 = new Person('Sean');
+  
+  
+  var helloFunction = person1.sayThisisapost
+  var helloFunction = person2.sayThisisapost
 
-person1.firstname = "Michael";
+person1.firstname = "Michael";       // Set up names for who the different people who post. But how?
+person2.firstname = "Joe" ;
+person3.firstname = "Natalia";
+person4.firstname = "Spoof";
+person5.firstname = "Dustin"; 
+person6.firstname = "Sean";
 console.log(person1.firstName)
 
- person1.thisisapost ; 
+  
+  console.log(person1.firstname + " This is my first post about boxes")    // Set up how I'm going to output the names and messages.
+  
+  console.log(person2.firstname + " This is also a post about boxes")
+  console.log(person3.firstname + " Boxes are dumb");
+  console.log(person4.firstname + " What are boxes?");
+  console.log(person5.firstname + " I like corn");
+  console.log(person6.firstname + " Fuel injected headers in my Audi");
+  
--- playtime.js
+++ playtime.js
@@ -1,10 +1,1 @@
 // JavaScript
-
-
-var list = [];
-list.push('one');
-list.push('two')
-console.log(list[0]);
-console.log(list[list.length-1]);
-list.pop();
-console.log(list[list.length-1]);
