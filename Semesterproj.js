c78e23cc30311f22e96c7961f6423bd345b0b100
mbates7 <hotfortaturtle@gmail.com>
2015-12-15 00:55:43 +0000

    Final project

--- Semesterproj.js
+++ Semesterproj.js
@@ -11,6 +11,8 @@ Person.prototype.sayHello = function() {
   var person4 = new Person('Spoof');
   var person5 = new Person('Dustin');
   var person6 = new Person('Sean');
+  var person7 = new Person('Rich');
+  var person8 = new Person('Josh');
   
   
   var helloFunction = person1.sayThisisapost
@@ -22,14 +24,17 @@ person3.firstname = "Natalia";
 person4.firstname = "Spoof";
 person5.firstname = "Dustin"; 
 person6.firstname = "Sean";
+person7.firstname = "Rich";
+person8.firstname = "Josh";
 console.log(person1.firstName)
 
   
   console.log(person1.firstname + " This is my first post about boxes")    // Set up how I'm going to output the names and messages.
   
   console.log(person2.firstname + " This is also a post about boxes")
-  console.log(person3.firstname + " Boxes are dumb");
-  console.log(person4.firstname + " What are boxes?");
+  console.log(person3.firstname + " @Michael Boxes are dumb");                       // Don't know how I'm going to set up reply messages 
+  console.log(person4.firstname + " @Michael What are boxes?");                // Using the @ symbol I can signify I'm replying
   console.log(person5.firstname + " I like corn");
-  console.log(person6.firstname + " Fuel injected headers in my Audi");
-  
+  console.log(person6.firstname + " I have fuel injected headers in my Audi");
+  console.log(person7.firstname + " I hate playing Counter Strike because I get mad");
+  console.log(person8.firstname + " I vape");
